aec594719b7b93b39ad701762d6920fad0b21a1d
Pooya Khorrami <pkhorrami4@gmail.com>
2014-09-24 13:04:20 -0500

    Modified log_plotter.py to plot test error if present.

--- scripts/log_plotter.py
+++ scripts/log_plotter.py
@@ -2,17 +2,23 @@ import argparse
 from matplotlib import pyplot
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(prog='log plotter', description='Script to plot log data')
-	parser.add_argument('log_path', help='Path to log.txt')
+    parser = argparse.ArgumentParser(prog='log plotter', description='Script to plot log data')
+    parser.add_argument('log_path', help='Path to log.txt')
 
-	args = parser.parse_args()
-	log_path = args.log_path
+    args = parser.parse_args()
+    log_path = args.log_path
 
-	file = open(log_path, 'r')
-	lines = file.readlines()
-	file.close()
+    file = open(log_path, 'r')
+    lines = file.readlines()
+    file.close()
 
-	error = [float(line.split(',')[1].split(':')[1].split(' ')[1]) for line in lines if line.startswith('*')]
+    train_error = [float(line.split(',')[1].split(':')[1].split(' ')[1]) for line in lines if line.startswith('*')]
+    test_error = [float(line.split(',')[1].split(':')[1].split(' ')[1]) for line in lines if line.startswith('&')] 
 
-	pyplot.plot(error)
-	pyplot.show()
+    pyplot.plot(train_error, label='Train')
+    if test_error:
+        pyplot.plot(test_error, label='Test')
+    pyplot.xlabel('Steps / 50')
+    pyplot.ylabel('Error')
+    pyplot.legend()
+    pyplot.show()

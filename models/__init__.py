5340f2552e76e08d3dbf1dd2ced4856aa78d8c84
Tom Le Paine <tom.le.paine@gmail.com>
2014-11-18 21:14:43 -0600

    Added regression model.

--- models/__init__.py
+++ models/__init__.py
@@ -160,6 +160,58 @@ class SupervisedModel(AbstractModel):
         accuracy_symbol = 1.0*T.sum(T.eq(
             predicted_label_symbol, y))/y.shape[0]
         return accuracy_symbol
+
+class RegressionModel(AbstractModel):  
+    def __init__(self, name, path, learning_rate=0.000001):
+        self.y = T.lvector(name='target')
+        super(RegressionModel, self).__init__(
+            name, path, learning_rate=learning_rate)
+
+    def _compile(self):
+        self.all_trainable_parameters_symbol = layers.all_trainable_parameters(
+            self._get_output_layer())
+
+        self.all_save_parameters_symbol = layers.all_parameters(
+            self._get_output_layer())
+
+        self.updates_symbol = layers.gen_updates_regular_momentum(
+            self._get_cost_symbol(),
+            self.all_trainable_parameters_symbol,
+            learning_rate=self.learning_rate_symbol,
+            momentum=0.9,
+            weight_decay=1e-5)
+        
+        self.train_func = theano.function(
+            [self._get_input_symbol(), self._get_y_symbol()],
+            [self._get_cost_symbol(), self._get_accuracy_symbol()],
+            updates=self.updates_symbol)
+                
+        self.eval_func = theano.function(
+            [self._get_input_symbol(), self._get_y_symbol()], 
+            self._get_accuracy_symbol())
+
+        self.prediction_func = theano.function(
+            [self._get_input_symbol()],
+            self._get_output_symbol())
+
+    def _get_cost_symbol(self):
+        y = self._get_y_symbol()
+        output = self._get_output_symbol()
+        cost = T.mean((y - output)**2)
+        return cost
+
+    def train(self, x_batch, y_batch):
+        return self.train_func(x_batch, y_batch)
+            
+    def eval(self, x_batch, y_batch):
+        return self.eval_func(x_batch, y_batch)
+
+    def prediction(self, x_batch):
+        return self.prediction_func(x_batch)
+
+    # New methods for this class     
+    def _get_y_symbol(self):
+        return self.y
     
 class ReinforcementModel(AbstractModel):
     def __init__(self, name, path, learning_rate=0.001):

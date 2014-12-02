d5eef055889cd8d4928f961030d285b1d346f85b
Tom Le Paine <tom.le.paine@gmail.com>
2014-12-01 23:37:49 -0600

    Added new cifar10 reduced experiment.

---
+++ experiments/cifar10_reduced/L3_3_192_t_0_L2_5_144_t_0_L1_5_96_t_0_lcn_dropout_s_500_data_augmentation/supervised/finetune/random/train.py
@@ -0,0 +1,57 @@
+import os
+import sys
+sys.path.append('../../..')
+
+import numpy
+
+from fastor import util
+from fastor.datasets import supervised_dataset
+
+from model import SupervisedModel
+
+print('Start')
+
+pid = os.getpid()
+print('PID: {}'.format(pid))
+f = open('pid', 'wb')
+f.write(str(pid)+'\n')
+f.close()
+
+model = SupervisedModel('experiment', './', learning_rate=1e-2)
+monitor = util.Monitor(model)
+
+# Loading CIFAR-10 dataset
+print('Loading Data')
+train_iterator = util.get_cifar_iterator_reduced('train',
+                                    mode='random_uniform',
+                                    batch_size=128,
+                                    num_batches=100000,
+                                    rescale=True,
+                                    num_samples_per_class=500, 
+                                    which_split=0)
+
+test_iterator = util.get_cifar_iterator('test',
+                                    mode='random_uniform',
+                                    batch_size=128,
+                                    num_batches=100000,
+                                    rescale=True)
+
+normer = util.Normer2(filter_size=5, num_channels=3)
+augmenter = util.DataAugmenter(2, (32, 32))
+
+print('Training Model')
+for x_batch, y_batch in train_iterator:     
+    x_batch = augmenter.run(x_batch)
+    x_batch = normer.run(x_batch)   
+    y_batch = numpy.int64(numpy.argmax(y_batch, axis=1))
+    monitor.start()
+    log_prob, accuracy = model.train(x_batch, y_batch)
+    monitor.stop(1-accuracy) # monitor takes error instead of accuracy    
+    
+    if monitor.test:
+        monitor.start()
+        x_test_batch, y_test_batch = test_iterator.next()
+        x_test_batch = normer.run(x_test_batch)
+        y_test_batch = numpy.int64(numpy.argmax(y_test_batch, axis=1))
+        test_accuracy = model.eval(x_test_batch, y_test_batch)
+        monitor.stop_test(1-test_accuracy)
